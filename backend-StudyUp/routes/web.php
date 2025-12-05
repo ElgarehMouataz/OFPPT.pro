f3bbc56a4d54822407f9e3341eb57b93ed46a65d
MouatazElgareh <mouatazelgareh044@gmail.com>
2025-12-05 14:11:01 +0100

    fixed laravel connection

--- backend-StudyUp/routes/web.php
+++ backend-StudyUp/routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-DB_CONNECTION=sqlsrv
-DB_HOST=localhost
-DB_PORT=1433
-DB_USERNAME='root'
-DB_PASSWORD=''
-DB_DATABASE='studyup'
-
 
 Route::middleware('api')->group(function () {
     Route::get('/annee', function () {
