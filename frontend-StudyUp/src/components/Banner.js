66b07ad5c21bfa2454b50d95c7483313c79a5c86
MouatazElgareh <mouatazelgareh044@gmail.com>
2026-01-01 18:36:07 +0100

    Update Banner.js

--- frontend-StudyUp/src/components/Banner.js
+++ frontend-StudyUp/src/components/Banner.js
@@ -3,10 +3,9 @@ export default function Banner() {
      const { colors, mode,toggleTheme } = useTheme()
     return (
       <div className="banner">
-        <div className="togglecontainer" style={{backgroundColor: colors.background, color: colors.text}}>
-        <button className="toggleButton" style={{...colors.icon}} onClick={toggleTheme}></button>
-        {mode === "light" ? "dark" : "light"} mode
-        </div>
+        <div className="togglecontainer"  onClick={toggleTheme} style={{backgroundColor: colors.background, color: colors.text}}>
+          <button className="toggleButton" style={{...colors.icon}}></button>
+          {mode === "light" ? "dark" : "light"} mode</div>
         </div>
     )
   }
